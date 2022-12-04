cbfa4bc4e8c6dc5b0f4345303da92f79e513a376
Harshwardhan Raghunath Patil <43771203+harshwardhanp@users.noreply.github.com>
2022-12-04 13:39:35 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+$(function () {
+    $("#search").click(function () {
+        $("form").submit(function (e) {
+            e.preventDefault();
+        });
+        console.log($(document.activeElement).val());
+        console.log('Output: I got executed');
+        var key = $("#keywordSearch").val();
+        key = key.toLowerCase();
+        var searchres = "We found " + key + " in";
+        var a = key;
+        $.getJSON("reducer_output.json",
+            function (data) {
+                var student = '';
+                // ITERATING THROUGH OBJECTS
+                $.each(data, function (key, value) {
+
+                    //CONSTRUCTION OF ROWS HAVING
+                    // DATA FROM JSON OBJECT
+                    student += '<tr>';
+                    student += '<td>' +
+                        key + '</td>';
+
+                    student += '</tr>';
+                });
+
+                //INSERTING ROWS INTO TABLE 
+                $('#table').append(student);
+        });
+    });
+});
